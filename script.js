2cd4c14dcd8f59d9dbadfefe513fc90bfbdd63dc
Naji Dia <najimdia@gmail.com>
2021-11-04 17:55:25 -0700

    comit

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ var submitBtn = $("#submit").on("click", function () {
 function yelpCard(data) {
   for (var i = 0; i < data.length; i++) {
     $("#cards").append(`
-    <a id="yelpTitle" class="card-header" href="${data[i].url}" target="_blank">${data[i].name}
+    <a id="yelpTitle" class="card-title col-4" href="${data[i].url}" target="_blank">${data[i].name}
     <img id="yelpImg" class="card-img-top" src="${data[i].image_url}">
     <p id="rating" class="card-text">Rating: ${data[i].rating}</p>
     <p id="address" class="card-text">Address: ${data[i].location.display_address.join(" ")}</p>
